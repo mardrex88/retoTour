384eb65c2176adfda31fdaf1165589dfc8a85414
Marlon Vargas <mardrex88@gmail.com>
2022-07-14 09:42:24 -0500

    Se cran las colecciones Team y Cyclist

--- src/main/java/com/mardrex/reto/collections/Cyclist.java
+++ src/main/java/com/mardrex/reto/collections/Cyclist.java
@@ -2,6 +2,7 @@ package com.mardrex.reto.collections;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -9,5 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 public class Cyclist {
 
+    @Id
+    private String id;
+    private String name;
+    private Integer numberId;
+    private Team team;
+    private String country;
 
 }
--- src/main/java/com/mardrex/reto/collections/Team.java
+++ src/main/java/com/mardrex/reto/collections/Team.java
@@ -1,4 +1,24 @@
 package com.mardrex.reto.collections;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Getter
+@Setter
+@Builder(toBuilder = true)
 public class Team {
+
+    @Id
+    private String id;
+    private String name;
+    private String code;
+    private String country;
+    private List<Cyclist> cyclists;
+
 }
